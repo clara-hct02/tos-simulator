57ea66a2acd0bfe7c72ab2ac2ee13fdcac02def3
clara-hct02 <clarahct1102@gmail.com>
2026-06-27 15:18:58 -0700

    add win page

---
+++ frontend/src/win.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import { useGame } from '../src/context/GameContext';
+
+const Win = () => {
+  const { game } = useGame();
+  const navigate = useNavigate();
+
+    
+  return (
+    <div className="game-container">
+      <h1> {game.winner} Wins!! </h1>
+
+        <div className="button-group">
+          <button onClick={() => navigate('/')}>
+            Back
+          </button>
+
+        </div>
+      </div>
+    );
+};
+
+export default Win;

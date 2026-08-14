72d6ca512ca4855e8602935e3d53b611624abced
clara-hct02 <clarahct1102@gmail.com>
2026-08-14 10:47:16 -0700

    fix: deleted file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
 import App from './App.jsx'
 import Rules from './rules.jsx'
 import About from './about.jsx'
